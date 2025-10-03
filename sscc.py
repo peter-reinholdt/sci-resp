df0d030c97e877641442e23cde6c9d5a06629dfb
Peter Reinholdt <peter.reinholdt@gmail.com>
2025-10-03 14:06:54 +0200

    Remove timing information

--- sscc.py
+++ sscc.py
@@ -12,9 +12,6 @@ from one_electron_operator import singlet_operator_state, triplet_operator_state
 from pyscf.data.gyro import get_nuc_g_factor
 from pyscf.data import nist
 import scipy
-import mkl
-import sparse_dot_mkl
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=int, required=True)
@@ -39,56 +36,18 @@ def _make_rdm1_on_mo(casdm1, ncore, ncas, nmo):
     dm1[ncore:nocc,ncore:nocc] = casdm1
     return dm1
 
-# workaround for working with sparse_dot_mkl when only LP64 (32-bit) MKL interface available
-# we split the matrix up into small enough (<2**31-1) sized blocks
-# so it fits
-
-def csr_blocks(A, max_nnz):
-    if A.nnz < max_nnz:
-        return [A]
-    n = A.shape[0]
-    assert A.shape[0] == A.shape[1]
-    indptr = A.indptr
-    start_row = 0
-    blocks = []
-    while start_row < n:
-        nnz_in_block = 0
-        end_row = start_row
-        while end_row < n and nnz_in_block + (indptr[end_row+1] - indptr[end_row]) <= max_nnz:
-            nnz_in_block += indptr[end_row+1] - indptr[end_row]
-            end_row += 1
-        A_block = A[start_row:end_row, :]
-        blocks.append(A_block)
-        start_row = end_row
-    return blocks
-
-def blocked_spmv(A_blocks, x):
-    y = np.zeros(x.shape, dtype=x.dtype)
-    start_row = 0
-    for A_block in A_blocks:
-        end_row = start_row + A_block.shape[0]
-        y[start_row:end_row] = sparse_dot_mkl.sparse_dot.dot_product_mkl(A_block, x)
-        start_row = end_row
-    return y
-
-def pyci_op_to_csr(op, max_nnz=2**31-1):
-    indices = op.indices()
-    indptr = op.indptr()
-    data = op.data()
-    matrix = scipy.sparse.csr_array((data, indices, indptr))
-    print('INFO: nnz=', matrix.nnz)
-    diagonal = matrix.diagonal()
-    matrix += matrix.T
-    matrix -= scipy.sparse.diags(diagonal)
-    matrix_blocks = csr_blocks(matrix, max_nnz=max_nnz)
-    matvec = lambda v: blocked_spmv(matrix_blocks, v)
-    return matrix, diagonal, matvec
-
-
-t1 = time.time()
+def wrap_matvec(op, x):
+    if x.ndim == 1:
+        return op.matvec(x)
+    elif x.ndim == 2:
+        out = np.zeros_like(x)
+        for i in range(x.shape[1]):
+            out[:,i] = op.matvec(x[:,i])
+        return out
 
 xyz = 'water.xyz'
-basis = 'aug-cc-pVTZ-J'
+basis = '6-311++Gss-J'
+nuc_pair = [(1,2), (1,0)]
 
 m = pyscf.M(atom=xyz, basis=basis, symmetry=True)
 
@@ -97,7 +56,6 @@ ncas = m.nao
 nelcas = sum(m.nelec)
 nelec = m.nelec
 
-nuc_pair = [(1,2)]
 m.max_memory = 3000 # 3 GB
 mf = pyscf.scf.RHF(m).run()
 mf.conv_tol = 1e-12
@@ -109,10 +67,7 @@ eri = pyscf.ao2mo.full(m, mf.mo_coeff[:, ncore:ncore+ncas], aosym='1').reshape(n
 ham = pyci.hamiltonian(ecore, h1, eri.transpose(0,2,1,3))
 
 wfn = pyci.fullci_wfn(ham.nbasis, *nelec)
-# hci wave function
 wfn.add_hartreefock_det()
-#da = np.array([0b111110, 0b11111])
-#wfn.add_det(da)
 dets_added = 1
 op = pyci.sparse_op(ham, wfn)
 e_vecs = np.array([[1.]])
@@ -120,40 +75,23 @@ e_vals = op.get_element(0,0) + op.ecore
 old_energy = np.min(e_vals)
 niter = 0
 
-
 # 1) Solve for |Psi_0>
 eps = args.eps
 eps_mu = eps
 eps_resp = eps
 
-t2 = time.time()
-print('timing::setup', t2 - t1)
-t1 = time.time()
-
-
 dets_added = True
 while dets_added:
     # Add connected determinants to wave function via HCI
     dets_added = pyci.add_hci(ham, wfn, e_vecs[0], eps=eps)
-    t2 = time.time()
-    print('timing::gs::add_hci', t2 - t1)
-    t1 = time.time()
     # Update CI matrix operator
     op.update(ham, wfn)
-    t2 = time.time()
-    print('timing::gs::op_update', t2 - t1)
-    t1 = time.time()
     # Solve CI matrix problem
     e_vecs = np.concatenate([e_vecs, np.zeros((dets_added, e_vecs.shape[1]))], axis=0)
-    matrix, hdiag, matvec = pyci_op_to_csr(op)
-    t2 = time.time()
-    print('timing::gs::op_transfer', t2 - t1)
-    t1 = time.time()
+    matvec = lambda v: wrap_matvec(op, v)
+    hdiag = op.diagonal()
     e_vals, e_vecs = solve_ci(matvec, hdiag, roots=1, c0=e_vecs, verbose=True)
     e_vals += op.ecore
-    t2 = time.time()
-    print('timing::gs::op_solve', t2 - t1)
-    t1 = time.time()
     delta_e = old_energy - np.min(e_vals)
     old_energy = np.min(e_vals)
     niter += 1
@@ -182,37 +120,23 @@ def resp(ham, nelec, gs_wfn, gs_evecs, integral, omega, gamma, model=5, triplet=
     wfn.add_dets_from_wfn(gs_wfn)
     op = pyci.sparse_op(ham, wfn)
     op.update(ham, wfn)
-    matrix, hdiag, matvec = pyci_op_to_csr(op)
+    matvec = lambda v: wrap_matvec(op, v)
+    hdiag = op.diagonal()
     e_vals, e_vecs = solve_ci(matvec, hdiag, roots=1, c0=None, verbose=True)
     e_vals += op.ecore
     old_energy = np.min(e_vals)
     mu =  one_electron_operator_for_cas(cas, integral)
     ham_mu = pyci.hamiltonian(0., mu, eri*0)
 
-    t2 = time.time()
-    print('timing::rsp::setup', t2 - t1)
-    t1 = time.time()
-
     if couple_property:
         # Add determinants connecting via the one-electron operator, and re-solve
         dets_added = pyci.add_hci(ham_mu, wfn, e_vecs[0], eps=eps_mu)
-        t2 = time.time()
-        print('timing::rsp-couple-property::add_hci', t2 - t1)
-        t1 = time.time()
         op.update(ham, wfn)
-        t2 = time.time()
-        print('timing::rsp-couple-property::op_update', t2 - t1)
-        t1 = time.time()
         e_vecs = np.concatenate((e_vecs, np.zeros((dets_added, e_vecs.shape[1]))), axis=0)
-        matrix, hdiag, matvec = pyci_op_to_csr(op)
-        t2 = time.time()
-        print('timing::rsp-couple-property::op_transfer', t2 - t1)
-        t1 = time.time()
+        matvec = lambda v: wrap_matvec(op, v)
+        hdiag = op.diagonal()
         e_vals, e_vecs = solve_ci(matvec, hdiag, roots=1, c0=e_vecs, verbose=True)
         e_vals += op.ecore
-        t2 = time.time()
-        print('timing::rsp-couple-property::op_solve', t2 - t1)
-        t1 = time.time()
         delta_e = old_energy - np.min(e_vals)
         old_energy = np.min(e_vals)
         num_determinants = e_vecs.shape[0]
@@ -224,9 +148,6 @@ def resp(ham, nelec, gs_wfn, gs_evecs, integral, omega, gamma, model=5, triplet=
     zPsi = property_operator(wfn.to_det_array(), mu, e_vecs[:,0])
     zPsi -= e_vecs[:,0] * np.dot(e_vecs[:,0], zPsi)
 
-    t2 = time.time()
-    print('timing::rsp::zPsi', t2 - t1)
-    t1 = time.time()
     if couple_zPsi:
         raise ValueError
     
@@ -234,48 +155,28 @@ def resp(ham, nelec, gs_wfn, gs_evecs, integral, omega, gamma, model=5, triplet=
     # 4) Solve response equation
     E0w = E0 + omega + 1j * gamma
     x = davidson_response(lambda v: matvec(v)-E0w*v, zPsi, hdiag-E0w, verbose=False)
-    t2 = time.time()
-    print('timing::rsp::davidson', t2 - t1)
-    t1 = time.time()
 
     # resolve with more added
     if couple_response:
         while True:
             dets_added = pyci.add_hci(ham, wfn, x, eps=eps_resp)
-            t2 = time.time()
-            print('timing::rsp-couple::add_hci', t2 - t1)
-            t1 = time.time()
             op.update(ham, wfn)
-            t2 = time.time()
-            print('timing::rsp-couple::op_update', t2 - t1)
-            t1 = time.time()
             #mu_op.update(ham_mu, wfn)
             e_vecs = np.concatenate((e_vecs, np.zeros((dets_added, e_vecs.shape[1]))), axis=0)
-            matrix, hdiag, matvec = pyci_op_to_csr(op)
-            t2 = time.time()
-            print('timing::rsp-couple::op_transfer', t2 - t1)
-            t1 = time.time()
+            matvec = lambda v: wrap_matvec(op, v)
+            hdiag = op.diagonal()
             e_vals, e_vecs = solve_ci(matvec, hdiag, roots=1, c0=e_vecs, verbose=True)
             e_vals += op.ecore
-            t2 = time.time()
-            print('timing::rsp-couple::op_solve', t2 - t1)
-            t1 = time.time()
             delta_e = old_energy - np.min(e_vals)
             old_energy = np.min(e_vals)
             num_determinants = e_vecs.shape[0]
             #zPsi = mu_op.matvec(e_vecs)
             zPsi = property_operator(wfn.to_det_array(), mu, e_vecs[:,0])
             zPsi -= e_vecs[:,0] * np.dot(e_vecs[:,0], zPsi)
-            t2 = time.time()
-            print('timing::rsp-couple::zPsi', t2 - t1)
-            t1 = time.time()
             E0 = np.dot(e_vecs[:,0], matvec(e_vecs[:,0]))
             E0w = E0 + omega + 1j * gamma
             x0 = np.concatenate((x, np.zeros(dets_added)))
             x = davidson_response(lambda v: matvec(v)-E0w*v, zPsi, hdiag-E0w, verbose=False, guess=x0)
-            t2 = time.time()
-            print('timing::rsp-couple::davidson', t2 - t1)
-            t1 = time.time()
             if dets_added / num_determinants < 0.01:
                 print(f'finised_couple_response {eps=} {e_vals[0]=} {delta_e=} {dets_added=} {num_determinants=}')
                 break
@@ -343,32 +244,22 @@ def response_dot(response_wfn, response_vector, response_e_vecs, h1ao, triplet=N
     # evaluate <X|B>; form property vector B from the determinant set used in response vector X
     property_operator = triplet_operator_state if triplet else singlet_operator_state
     mu =  one_electron_operator_for_cas(cas, h1ao)
-    property_vector = property_operator(response_wfn.to_det_array(), mu, response_e_vecs[0])
+    property_vector = property_operator(response_wfn.to_det_array(), mu, response_e_vecs.ravel())
+    property_vector -= response_e_vecs.ravel() * np.dot(property_vector, response_e_vecs.ravel())
     return np.dot(response_vector, property_vector)
 
 # SSCC - DSO (expectation value)
 e11_dso = np.zeros((len(nuc_pair), 3, 3))
 d1, d2 = pyci.compute_rdms(wfn, e_vecs[0])
 
-t2 = time.time()
-print('timing::compute_rdms', t2 - t1)
-t1 = time.time()
-
 rdm1 = d1[0] + d1[1]
 rdm1 = _make_rdm1_on_mo(rdm1, ncore, ncas, m.nao)
 rdm1_ao = mf.mo_coeff @ rdm1 @ mf.mo_coeff.T
-t2 = time.time()
-print('timing::rdm_mo2ao', t2 - t1)
-t1 = time.time()
 for k, (i,j) in enumerate(nuc_pair):
     dso_ao = dso_integral(m, m.atom_coord(i), m.atom_coord(j))
     a11 = -np.einsum('xymn,mn->xy', dso_ao, rdm1_ao)
     a11 = a11 - a11.trace() * np.eye(3)
     e11_dso[k] = a11
-
-t2 = time.time()
-print('timing::dso_dots', t2 - t1)
-t1 = time.time()
 
 # SSCC - PSO (response)
 e11_pso = np.zeros((len(nuc_pair), 3, 3))
@@ -377,17 +268,18 @@ response_e_vecs = []
 response_wfns = []
 h1aos = []
 for ia in range(m.natm):
-    if not any([ia in pair for pair in nuc_pair]):
-        # skip response on this nucleus
-        for i in range(3):
-            response_wfns.append(None)
-            response_e_vecs.append(None)
-            response_vectors.append(None)
-            h1aos.append(None)
-        continue
     m.set_rinv_origin(m.atom_coord(ia))
     h1ao = m.intor_asymmetric('int1e_prinvxp', 3)
     for operator in h1ao.reshape(3, m.nao, m.nao):
+        # we can skip response on this nucleus if it is not needed
+        # only the i'th perturbation in (i,j) nuc_pair is required
+        # we still need to save the operator for the property gradient
+        if not any([ia in pair[:1] for pair in nuc_pair]):
+            response_wfns.append(None)
+            response_e_vecs.append(None)
+            response_vectors.append(None)
+            h1aos.append(operator)
+            continue
         wfn_rsp, response_e_vec, response_vector, property_vector = resp(ham, nelec, wfn, e_vecs, operator, omega=0.0, gamma=0.0, model=args.model, triplet=False)
         response_wfns.append(wfn_rsp)
         response_e_vecs.append(response_e_vec)
@@ -412,14 +304,6 @@ response_wfns = []
 h1aos = []
 
 for ia in range(m.natm):
-    if not any([ia in pair for pair in nuc_pair]):
-        # skip response on this nucleus
-        for i in range(9):
-            response_wfns.append(None)
-            response_e_vecs.append(None)
-            response_vectors.append(None)
-            h1aos.append(None)
-        continue
     m.set_rinv_origin(m.atom_coord(ia))
     a01p = nist.G_ELECTRON * 0.25 * m.intor('int1e_sa01sp', 12).reshape(3,4,m.nao,m.nao)
     h1ao = -(a01p[:,:3] + a01p[:,:3].transpose(0,1,3,2))
@@ -430,6 +314,12 @@ for ia in range(m.natm):
     fc = 8*np.pi/3 * np.einsum('ip,iq->pq', ao, ao) * (nist.G_ELECTRON/2) / 2
     h1ao -= np.einsum('xy,mn->xymn', np.eye(3), fc)
     for i, operator in enumerate(h1ao.reshape(9, m.nao, m.nao)):
+        if not any([ia in pair[:1] for pair in nuc_pair]):
+            response_wfns.append(None)
+            response_e_vecs.append(None)
+            response_vectors.append(None)
+            h1aos.append(operator)
+            continue
         wfn_rsp, response_e_vec, response_vector, property_vector = resp(ham, nelec, wfn, e_vecs, operator, omega=0.0, gamma=0.0, model=args.model, triplet=True)
         response_wfns.append(wfn_rsp)
         response_e_vecs.append(response_e_vec)
@@ -456,17 +346,16 @@ response_e_vecs = []
 response_wfns = []
 h1aos = []
 for ia in range(m.natm):
-    if not any([ia in pair for pair in nuc_pair]):
-        # skip response on this nucleus
-        response_wfns.append(None)
-        response_e_vecs.append(None)
-        response_vectors.append(None)
-        h1aos.append(None)
-        continue
     coords = m.atom_coord(ia).reshape(1, 3)
     ao = m.eval_gto('GTOval', coords)
     h1ao = 8*np.pi/3 * np.einsum('ip,iq->pq', ao, ao) * (nist.G_ELECTRON/2) / 2
     operator = h1ao
+    if not any([ia in pair[:1] for pair in nuc_pair]):
+        response_wfns.append(None)
+        response_e_vecs.append(None)
+        response_vectors.append(None)
+        h1aos.append(operator)
+        continue
     wfn_rsp, response_e_vec, response_vector, property_vector = resp(ham, nelec, wfn, e_vecs, operator, omega=0.0, gamma=0.0, model=args.model, triplet=True)
     response_wfns.append(wfn_rsp)
     response_e_vecs.append(response_e_vec)
