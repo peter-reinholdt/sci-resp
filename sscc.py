a4c21e40a55611a2312288d64a75577f7c340fad
Peter Reinholdt <peter.reinholdt@gmail.com>
2025-11-18 14:12:42 +0100

    Cleanup

--- sscc.py
+++ sscc.py
@@ -6,53 +6,27 @@ import argparse
 import pyscf
 import numpy as np
 import pyci
-from functools import reduce
-from solvers import davidson_response, solve_ci
-from one_electron_operator import singlet_operator_state, triplet_operator_state
+from solvers import solve_ci
+from response import resp, wrap_matvec, response_dot, _make_rdm1_on_mo
 from pyscf.data.gyro import get_nuc_g_factor
 from pyscf.data import nist
-import scipy
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--xyz', type=str, required=True)
+parser.add_argument('--basis', type=str, required=True)
+parser.add_argument('--ncore', type=int, default=0)
 parser.add_argument('--couple-property', type=bool, default=True)
 parser.add_argument('--couple-response', type=bool, default=True)
 parser.add_argument('--eps', type=float, default=1e-3)
 args = parser.parse_args()
 
-
-def one_electron_operator_for_cas(casci, operator, mo_coeff=None, ncas=None, ncore=None):
-    # based on the pyscf thing
-    if mo_coeff is None: mo_coeff = casci.mo_coeff
-    if ncas is None: ncas = casci.ncas
-    if ncore is None: ncore = casci.ncore
-    mo_cas = mo_coeff[:,ncore:ncore+ncas]
-    h1 = reduce(np.dot, (mo_cas.conj().T, operator, mo_cas))
-    return h1
-
-def _make_rdm1_on_mo(casdm1, ncore, ncas, nmo):
-    nocc = ncas + ncore
-    dm1 = np.zeros((nmo,nmo))
-    idx = np.arange(ncore)
-    dm1[idx,idx] = 2
-    dm1[ncore:nocc,ncore:nocc] = casdm1
-    return dm1
-
-def wrap_matvec(op, x):
-    if x.ndim == 1:
-        return op.matvec(x)
-    elif x.ndim == 2:
-        out = np.zeros_like(x)
-        for i in range(x.shape[1]):
-            out[:,i] = op.matvec(x[:,i])
-        return out
-
-xyz = 'water.xyz'
-basis = '6-31G-J'
-nuc_pair = [(1,2), (1,0)]
+xyz = args.xyz
+basis = args.basis
 
 m = pyscf.M(atom=xyz, basis=basis, symmetry=True)
+ncore = args.ncore
+nuc_pair = [(i,j) for i in range(m.natm) for j in range(i)]
 
-ncore = 0
 ncas = m.nao
 nelcas = sum(m.nelec)
 nelec = m.nelec
@@ -99,74 +73,6 @@ while dets_added:
     niter += 1
     num_determinants = e_vecs.shape[0]
     print(f'{niter=} {eps=} {e_vals[0]=} {delta_e=} {dets_added=} {num_determinants=}')
-
-
-def resp(ham, nelec, gs_wfn, gs_evecs, integral, omega, gamma, couple_property=True, couple_response=True, triplet=False):
-    property_operator = triplet_operator_state if triplet else singlet_operator_state
-    
-    # setup common stuff
-    wfn = pyci.fullci_wfn(ham.nbasis, *nelec)
-    wfn.add_dets_from_wfn(gs_wfn)
-    op = pyci.sparse_op(ham, wfn)
-    op.update(ham, wfn)
-    matvec = lambda v: wrap_matvec(op, v)
-    hdiag = op.diagonal()
-    e_vals, e_vecs = solve_ci(matvec, hdiag, roots=1, c0=None, verbose=True)
-    e_vals += op.ecore
-    old_energy = np.min(e_vals)
-    mu =  one_electron_operator_for_cas(cas, integral)
-    ham_mu = pyci.hamiltonian(0., mu, eri*0)
-
-    if couple_property:
-        # Add determinants connecting via the one-electron operator, and re-solve
-        dets_added = pyci.add_hci(ham_mu, wfn, e_vecs[0], eps=eps_mu)
-        op.update(ham, wfn)
-        e_vecs = np.concatenate((e_vecs, np.zeros((dets_added, e_vecs.shape[1]))), axis=0)
-        matvec = lambda v: wrap_matvec(op, v)
-        hdiag = op.diagonal()
-        e_vals, e_vecs = solve_ci(matvec, hdiag, roots=1, c0=e_vecs, verbose=True)
-        e_vals += op.ecore
-        delta_e = old_energy - np.min(e_vals)
-        old_energy = np.min(e_vals)
-        num_determinants = e_vecs.shape[0]
-        print(f'couple_property {eps=} {e_vals[0]=} {delta_e=} {dets_added=} {num_determinants=}')
-
-    # Build z|Psi0>
-    #mu_op = pyci.sparse_op(ham_mu, wfn)
-    #zPsi = mu_op.matvec(e_vecs)
-    zPsi = property_operator(wfn.to_det_array(), mu, e_vecs[:,0])
-    zPsi -= e_vecs[:,0] * np.dot(e_vecs[:,0], zPsi)
-
-    E0 = np.dot(e_vecs[:,0], matvec(e_vecs[:,0]))
-    # 4) Solve response equation
-    E0w = E0 + omega + 1j * gamma
-    x = davidson_response(lambda v: matvec(v)-E0w*v, zPsi, hdiag-E0w, verbose=False)
-
-    # resolve with more added
-    if couple_response:
-        while True:
-            dets_added = pyci.add_hci(ham, wfn, x, eps=eps_resp)
-            op.update(ham, wfn)
-            e_vecs = np.concatenate((e_vecs, np.zeros((dets_added, e_vecs.shape[1]))), axis=0)
-            matvec = lambda v: wrap_matvec(op, v)
-            hdiag = op.diagonal()
-            e_vals, e_vecs = solve_ci(matvec, hdiag, roots=1, c0=e_vecs, verbose=True)
-            e_vals += op.ecore
-            delta_e = old_energy - np.min(e_vals)
-            old_energy = np.min(e_vals)
-            num_determinants = e_vecs.shape[0]
-            zPsi = property_operator(wfn.to_det_array(), mu, e_vecs[:,0])
-            zPsi -= e_vecs[:,0] * np.dot(e_vecs[:,0], zPsi)
-            E0 = np.dot(e_vecs[:,0], matvec(e_vecs[:,0]))
-            E0w = E0 + omega + 1j * gamma
-            x0 = np.concatenate((x, np.zeros(dets_added)))
-            x = davidson_response(lambda v: matvec(v)-E0w*v, zPsi, hdiag-E0w, verbose=False, guess=x0)
-            if dets_added / num_determinants < 0.01:
-                print(f'finised_couple_response {eps=} {e_vals[0]=} {delta_e=} {dets_added=} {num_determinants=}')
-                break
-            else:
-                print(f'couple_response {eps=} {e_vals[0]=} {delta_e=} {dets_added=} {num_determinants=}')
-    return wfn, e_vecs, x, zPsi
 
 def dso_integral(mol, orig1, orig2):
     '''Integral of vec{r}vec{r}/(|r-orig1|^3 |r-orig2|^3)
@@ -223,15 +129,6 @@ def convert_unit(e11):
     jtensor = np.einsum('ij,i,j->ij', ktensor, gyro, gyro)
     return jtensor
 
-def response_dot(response_wfn, response_vector, response_e_vecs, h1ao, triplet=None):
-    #             ~
-    # evaluate <X|B>; form property vector B from the determinant set used in response vector X
-    property_operator = triplet_operator_state if triplet else singlet_operator_state
-    mu =  one_electron_operator_for_cas(cas, h1ao)
-    property_vector = property_operator(response_wfn.to_det_array(), mu, response_e_vecs.ravel())
-    property_vector -= response_e_vecs.ravel() * np.dot(property_vector, response_e_vecs.ravel())
-    return np.dot(response_vector, property_vector)
-
 # SSCC - DSO (expectation value)
 e11_dso = np.zeros((len(nuc_pair), 3, 3))
 d1, d2 = pyci.compute_rdms(wfn, e_vecs[0])
@@ -264,7 +161,7 @@ for ia in range(m.natm):
             response_vectors.append(None)
             h1aos.append(operator)
             continue
-        wfn_rsp, response_e_vec, response_vector, property_vector = resp(ham, nelec, wfn, e_vecs, operator, omega=0.0, gamma=0.0, couple_property=args.couple_property, couple_response=args.couple_response, triplet=False)
+        wfn_rsp, response_e_vec, response_vector, property_vector = resp(cas, ham, nelec, wfn, e_vecs, operator, omega=0.0, gamma=0.0, eps_mu=eps_mu, eps_resp=eps_resp, couple_property=args.couple_property, couple_response=args.couple_response, triplet=False)
         response_wfns.append(wfn_rsp)
         response_e_vecs.append(response_e_vec)
         response_vectors.append(response_vector)
@@ -278,7 +175,7 @@ for k, (i,j) in enumerate(nuc_pair):
     h1aos_j = h1aos[j*3:(j+1)*3]
     for x in range(3):
         for y in range(3):
-            e11_pso[k,x,y] += -2*response_dot(response_wfns_i[x], response_vectors_i[x], response_e_vecs_i[x], h1aos_j[y], triplet=False)
+            e11_pso[k,x,y] += -2*response_dot(cas, response_wfns_i[x], response_vectors_i[x], response_e_vecs_i[x], h1aos_j[y], triplet=False)
 
 # SSCC - SD (response)
 e11_sd = np.zeros((len(nuc_pair), 3, 3))
@@ -304,7 +201,7 @@ for ia in range(m.natm):
             response_vectors.append(None)
             h1aos.append(operator)
             continue
-        wfn_rsp, response_e_vec, response_vector, property_vector = resp(ham, nelec, wfn, e_vecs, operator, omega=0.0, gamma=0.0, couple_property=args.couple_property, couple_response=args.couple_response, triplet=True)
+        wfn_rsp, response_e_vec, response_vector, property_vector = resp(cas, ham, nelec, wfn, e_vecs, operator, omega=0.0, gamma=0.0, eps_mu=eps_mu, eps_resp=eps_resp, couple_property=args.couple_property, couple_response=args.couple_response, triplet=True)
         response_wfns.append(wfn_rsp)
         response_e_vecs.append(response_e_vec)
         response_vectors.append(response_vector)
@@ -321,7 +218,7 @@ for k, (i,j) in enumerate(nuc_pair):
             for w in range(3):
                 xw = 3*x+w
                 yw = 3*y+w
-                e11_sd[k,x,y] += -2*response_dot(response_wfns_i[xw], response_vectors_i[xw], response_e_vecs_i[xw], h1aos_j[yw], triplet=True)
+                e11_sd[k,x,y] += -2*response_dot(cas, response_wfns_i[xw], response_vectors_i[xw], response_e_vecs_i[xw], h1aos_j[yw], triplet=True)
 
 # FC
 e11_fc = np.zeros((len(nuc_pair), 3, 3))
@@ -340,7 +237,7 @@ for ia in range(m.natm):
         response_vectors.append(None)
         h1aos.append(operator)
         continue
-    wfn_rsp, response_e_vec, response_vector, property_vector = resp(ham, nelec, wfn, e_vecs, operator, omega=0.0, gamma=0.0, couple_property=args.couple_property, couple_response=args.couple_response, triplet=True)
+    wfn_rsp, response_e_vec, response_vector, property_vector = resp(cas, ham, nelec, wfn, e_vecs, operator, omega=0.0, gamma=0.0, eps_mu=eps_mu, eps_resp=eps_resp, couple_property=args.couple_property, couple_response=args.couple_response, triplet=True)
     response_wfns.append(wfn_rsp)
     response_e_vecs.append(response_e_vec)
     response_vectors.append(response_vector)
@@ -352,7 +249,7 @@ for k, (i,j) in enumerate(nuc_pair):
     response_wfns_i = response_wfns[i]
     response_e_vecs_i = response_e_vecs[i]
     h1aos_j = h1aos[j]
-    val = -2*response_dot(response_wfns_i, response_vectors_i, response_e_vecs_i, h1aos_j, triplet=True)
+    val = -2*response_dot(cas, response_wfns_i, response_vectors_i, response_e_vecs_i, h1aos_j, triplet=True)
     e11_fc[k,0,0] = e11_fc[k,1,1] = e11_fc[k,2,2] = val
 
 
